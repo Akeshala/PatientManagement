62b9f7d5adf40c18aca30ad1857ff691f07ced31
Akeshala <chavinduakeshala@gmail.com>
2023-12-31 19:39:23 +0530

    cloudfront images

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import "./App.css";
 
 function App() {
 
-  const images = ['image1.jpeg', 'image2.jpeg', 'image3.jpeg', 'image4.webp'];
+  const images: string[] = ['image1.jpeg', 'image2.jpeg', 'image3.jpeg', 'image4.webp'];
   return (
     <div className="App">
       <h1>{"Wildlife Photography"}</h1>
       <div>
-        {images.map((image, index) => (
+        {images.map((image: string, index : number) => (
             <img
                 key={index}
-                src={`https://d1da5ij22hqcih.cloudfront.net/${image}`}
+                src={`https://d1tgu70bdghfg5.cloudfront.net/images/${image}`}
                 alt={`Image ${index}`}
                 style={{ width: 'auto', height: '150px', margin: '10px' }}
             />
