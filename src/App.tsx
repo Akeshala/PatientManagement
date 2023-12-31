db5180c101d18e1f780f0fb3823ad584c4a7c7dc
Akeshala <chavinduakeshala@gmail.com>
2023-12-31 17:09:36 +0530

    build folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,25 @@
-import { useEffect, useState } from "react";
-import {
-  ListObjectsCommand,
-  ListObjectsCommandOutput,
-  S3Client,
-} from "@aws-sdk/client-s3";
-import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
 import "./App.css";
 
 function App() {
-  const [objects, setObjects] = useState<
-    Required<ListObjectsCommandOutput>["Contents"]
-  >([]);
 
-  useEffect(() => {
-    const client = new S3Client({
-      region: "us-east-2",
-      credentials: fromCognitoIdentityPool({
-        clientConfig: { region: "us-east-2" },
-        identityPoolId: "us-east-2:15c170ed-a0ab-46ed-a333-42c1d4b0303e",
-      }),
-    });
-    const command = new ListObjectsCommand({ Bucket: "patientmanagement89" });
-    client.send(command).then(({ Contents }) => setObjects(Contents || []));
-  }, []);
+  const images = [
+    'https://patientmanagement89.s3.us-east-2.amazonaws.com/image1.jpeg',
+    'https://patientmanagement89.s3.us-east-2.amazonaws.com/image2.jpeg',
+    'https://patientmanagement89.s3.us-east-2.amazonaws.com/image3.jpeg',
+    'https://patientmanagement89.s3.us-east-2.amazonaws.com/image4.webp',
+  ];
+
 
   return (
     <div className="App">
-      <h1>{"Patient Images"}</h1>
+      <h1>{"Wildlife Photography"}</h1>
       <div>
-        {objects.map((o, index) => (
+        {images.map((image, index) => (
             <img
                 key={index}
-                src={`https://d1da5ij22hqcih.cloudfront.net/${o.Key}`}
+                src={image}
                 alt={`Image ${index}`}
-                style={{ width: '200px', height: 'auto', margin: '5px' }}
+                style={{ width: 'auto', height: '150px', margin: '10px' }}
             />
         ))}
       </div>
